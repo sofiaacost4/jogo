00d4a2d861b7cbae955056a7bbff8884f816c463
sofia <k.sofia@escolar.ifrn.edu.br>
2025-02-16 11:28:15 -0300

    "Masks"

--- jogo.py
+++ jogo.py
@@ -1,6 +1,7 @@
 import pygame
 import random
 
+
 class MeninaSprite(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
@@ -9,6 +10,7 @@ class MeninaSprite(pygame.sprite.Sprite):
         self.image = menina_img
         self.rect = menina_img.get_rect()
         self.rect.topleft = (210, 270)
+        self.mask = pygame.mask.from_surface(self.image) #cria uma máscara para aperfeiçoar a colisão
         self.velocidade = 4
         self.direcao = 0  # 0 - direita | 1 - esquerda
 
@@ -32,6 +34,7 @@ class MaçãSprite(pygame.sprite.Sprite):
         self.image = maçã_img
         self.rect = self.image.get_rect()
         self.rect.topleft = (random.choice([50, 150, 250, 350, 450, 550]), 70)
+        self.mask = pygame.mask.from_surface(self.image)
         self.velocidade = 2
         self.posicao = 0
 
@@ -82,6 +85,7 @@ x = 1
 y = 2
 prox_maçã_podre = 10
 
+
 #game loop
 while running:
     for event in pygame.event.get():
@@ -93,21 +97,22 @@ while running:
         menina.p_esquerda()
     if keys[pygame.K_RIGHT]:
         menina.p_direita()
+    
+    pygame.mouse.set_visible(False)
 
-    #verifica a colisão
-    hit_list = pygame.sprite.spritecollide(menina, sprites_maçãs, True)
-    hit_list_2 = pygame.sprite.spritecollide(menina, sprites_maçãs_podres, True)
+    #verifica a colisão com as máscaras
+    hit_list = pygame.sprite.spritecollide(menina, sprites_maçãs, True, pygame.sprite.collide_mask)
+    hit_list_2 = pygame.sprite.spritecollide(menina, sprites_maçãs_podres, True, pygame.sprite.collide_mask)
   
     all_sprites.update()
     
     #adiciona uma maçã podre no jogo a cada 10 maçãs coletadas
 
     if num == prox_maçã_podre:
-      prox_maçã_podre += 10  # Atualiza o próximo ponto de criação
+      prox_maçã_podre += 10
       maçãpodre = MaçãPodre()
       sprites_maçãs_podres.add(maçãpodre)
       all_sprites.add(maçãpodre)
-    
        
     if hit_list:
         num += len(hit_list)
@@ -119,7 +124,6 @@ while running:
         if vidas <= 0:
             running = False
 
-
     #adiciona novas maçãs quando o jogador coleta uma determinada quantidade de maçãs
     if len(sprites_maçãs) < y:
         maçãs = criar_maçãs(x)
@@ -127,17 +131,14 @@ while running:
             x = 2
             y = 3
         if num >= 80:
-            x = 3
             y = 4
         sprites_maçãs.add(maçãs)
         all_sprites.add(maçãs)
 
-    # Atualiza a tela
     screen.fill((255, 255, 255))
     screen.blit(fundo_img, (0, 0))
     all_sprites.draw(screen)
 
-    # Exibe o texto de quantidade de maçãs
     texto = font.render(f"Maçãs: {num}", True, 'white')
     screen.blit(texto, (20, 12))
     texto = font.render(f"Vidas: {vidas}", True, 'white')
